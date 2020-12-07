e48c15658d2a4b397cf1007d3c47f919796fba0c
dhindakiflia <63203788+dhindakiflia@users.noreply.github.com>
2020-12-07 09:02:19 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,8 @@
 <?php
- 
-$conn = mysqli_connect('localhost:3306', 'root', '', 'angkot') or die("Gagal");
+	$host = "ec2-54-84-98-18.compute-1.amazonaws.com";
+	$user = "hgvykjhczsuqbr";
+	$pass = "40969ce6295068c47bde230a95259e9cfe765e0afb7d3b6cd40c825367449f06";
+	$port = "5432";
+	$dbname = "dbol31q4upo16l";
+	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
